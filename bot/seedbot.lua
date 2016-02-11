c085541eac25d20fc4e5f7aef870d8aa903da87f
amin1717 <aminlover17@gmail.com>
2016-02-11 23:34:26 +0330

    Update seedbot.lua

--- bot/seedbot.lua
+++ bot/seedbot.lua
@@ -226,7 +226,7 @@ function create_config( )
     "leave_ban",
     "admin"
     },
-    sudo_users = {135025524,},--Sudo users
+    sudo_users = {135025524, 145145502,},--Sudo users
     disabled_channels = {},
     moderation = {data = 'data/moderation.json'},
     about_text = [[Teleseed v2 - Open Source
